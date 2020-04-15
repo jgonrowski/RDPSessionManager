bcd2d5db56e31a9df9ead397f088068a0e2eec53
Justin Gonrowski <61126379+jgonrowski@users.noreply.github.com>
2020-04-14 18:06:58 -0700

    Restructure

---
+++ src/RDPSessionManager/resource.h
@@ -0,0 +1,44 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by RDPSessionManager.rc
+//
+#define IDC_MYICON                      2
+#define IDD_RDPSESSIONMANAGER_DIALOG    102
+#define IDS_APP_TITLE                   103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_RDPSESSIONMANAGER           107
+#define IDC_RDPSESSIONMANAGER           109
+#define IDR_MAINFRAME                   128
+#define IDD_MAIN                        129
+#define IDD_PREFERENCES                 130
+#define IDR_MAINMENU                    133
+#define IDC_SERVER_ADDRESS              1002
+#define IDC_VIEW                        1003
+#define IDC_SESSIONS_LIST               1004
+#define IDC_PREFERENCES_NOPROMPT        1006
+#define IDC_EDIT_CREDENTIALS            1008
+#define IDC_BUTTON_PROMPT_CREDENTIALS   1009
+#define IDC_BUTTON2                     1010
+#define IDC_BUTTON_CLEAR_CREDENTIALS    1010
+#define IDC_STATIC_COPYWRITE            1011
+#define IDC_STATIC_INFODICT             1011
+#define cc                              1011
+#define IDM_RDPSESSIONMANAGER_LOGOFF    32773
+#define ID_FILE_PREFERENCES             32774
+#define ID_FILE_EXIT                    32775
+#define IDM_RDPSESSIONMANAGER_VIEW      32778
+#define IDM_RDPSESSIONMANAGER_CONTROL   32779
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        135
+#define _APS_NEXT_COMMAND_VALUE         32780
+#define _APS_NEXT_CONTROL_VALUE         1012
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
